c61fe562cddfbe083d19fb371c035a9a3051e633
Jun Chen <cjcj46262@gmail.com>
2025-10-10 13:58:28 +0800

    del

--- merge_beit3/script_in1k.sh
+++
@@ -1,4 +0,0 @@
-python merge_beit.py --model beit3_base_patch16_224 --task imagenet --batch_size \
-64 --sentencepiece_model ./model_weights/beit3.spm --data_path \
-YOUR_PATH_HERE \
---output_dir ./output --eval --dist_eval --device cuda:3
--- merge_beit3/script_retrieval.sh
+++
@@ -1,3 +0,0 @@
-python merge_beit.py --model beit3_base_patch16_384 --input_size 384 \
---task coco_retrieval --batch_size 16 --sentencepiece_model ./model_weights/beit3.spm \
---data_path YOUR_PATH_HERE --eval --dist_eval --device cuda:0
