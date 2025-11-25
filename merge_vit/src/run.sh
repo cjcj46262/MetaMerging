c24e8354069ca9f1dbe0b6ba86e45d1a2863f7d3
cjcj46262 <cjcj46262@gmail.com>
2025-11-26 01:39:21 +0800

    Replace with main_meta_merging.py

--- merge_vit/src/run.sh
+++ merge_vit/src/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # run_merge.sh
 
-python main_emr_merging.py --batch_size 128 --meta_batch_size 8 --meta_batch_size 4 --epoch 3000 --inner_steps 1 --metalr 1 --adalr 0.1 --gpu 2
-python main_emr_merging.py --batch_size 128 --meta_batch_size 8 --meta_batch_size 4 --epoch 3000 --inner_steps 1 --metalr 0.1 --adalr 0.1 --gpu 2
-python main_emr_merging.py --batch_size 128 --meta_batch_size 8 --meta_batch_size 4 --epoch 3000 --inner_steps 1 --metalr 0.01 --adalr 0.1 --gpu 2
-python main_emr_merging.py --batch_size 128 --meta_batch_size 8 --meta_batch_size 4 --epoch 3000 --inner_steps 1 --metalr 0.001 --adalr 0.1 --gpu 2
-python main_emr_merging.py --batch_size 128 --meta_batch_size 8 --meta_batch_size 4 --epoch 3000 --inner_steps 1 --metalr 0.01 --adalr 1 --gpu 2
-python main_emr_merging.py --batch_size 128 --meta_batch_size 8 --meta_batch_size 4 --epoch 3000 --inner_steps 1 --metalr 0.01 --adalr 0.1 --gpu 2
-python main_emr_merging.py --batch_size 128 --meta_batch_size 8 --meta_batch_size 4 --epoch 3000 --inner_steps 1 --metalr 0.01 --adalr 0.01 --gpu 2
-python main_emr_merging.py --batch_size 128 --meta_batch_size 8 --meta_batch_size 4 --epoch 3000 --inner_steps 1 --metalr 0.01 --adalr 0.001 --gpu 2
+python main_meta_merging.py --batch_size 128 --meta_batch_size 8 --meta_batch_size_test 4 --epoch 3000 --inner_steps 1 --metalr 1 --adalr 0.1 --gpu 2
+python main_meta_merging.py --batch_size 128 --meta_batch_size 8 --meta_batch_size_test 4 --epoch 3000 --inner_steps 1 --metalr 0.1 --adalr 0.1 --gpu 2
+python main_meta_merging.py --batch_size 128 --meta_batch_size 8 --meta_batch_size_test 4 --epoch 3000 --inner_steps 1 --metalr 0.01 --adalr 0.1 --gpu 2
+python main_meta_merging.py --batch_size 128 --meta_batch_size 8 --meta_batch_size_test 4 --epoch 3000 --inner_steps 1 --metalr 0.001 --adalr 0.1 --gpu 2
+python main_meta_merging.py --batch_size 128 --meta_batch_size 8 --meta_batch_size_test 4 --epoch 3000 --inner_steps 1 --metalr 0.01 --adalr 1 --gpu 2
+python main_meta_merging.py --batch_size 128 --meta_batch_size 8 --meta_batch_size_test 4 --epoch 3000 --inner_steps 1 --metalr 0.01 --adalr 0.1 --gpu 2
+python main_meta_merging.py --batch_size 128 --meta_batch_size 8 --meta_batch_size_test 4 --epoch 3000 --inner_steps 1 --metalr 0.01 --adalr 0.01 --gpu 2
+python main_meta_merging.py --batch_size 128 --meta_batch_size 8 --meta_batch_size_test 4 --epoch 3000 --inner_steps 1 --metalr 0.01 --adalr 0.001 --gpu 2
